7eb3e5531449d1ce02e76290cba5e107f5ad0132
OmerAsik <faruk.asikoglu@gmail.com>
2022-03-23 15:24:41 +0300

    Added: Affairs3D Environment

---
+++ Assets/Library/Scripts/Affairs3D/SheetController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using NaughtyAttributes;
+
+//gelecek komutlarýn sheet'i tutar
+namespace Affairs3D
+{
+    public class SheetController : MonoBehaviour
+    {
+        [SerializeField] private float pi;
+
+
+
+
+
+
+    }
+
+}
